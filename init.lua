8c84a596e57fac46a634f2e41ce74e64d33211e3
Lisandro Bertoli <lisandro.bertoli2@promocionesfarma.com>
2024-09-12 18:16:02 +0100

    fix config after update to nvim 0.11

--- init.lua
+++ init.lua
@@ -87,4 +87,9 @@ if not vim.loop.fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup 'plugins'
+require('lazy').setup('plugins', {
+    rocks = {
+        enabled = false,
+        hererocks = false,
+    },
+})
